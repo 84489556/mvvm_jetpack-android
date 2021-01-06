0562951a035e296398535d640392693aad2040f7
tangqianzhu@99114.com <472578439@qq.com>
2021-01-06 09:13:52 +0800

    观点页面开始搭建

--- app/src/main/java/com/yd/huixuangu/ui/activity/MainActivity.java
+++ app/src/main/java/com/yd/huixuangu/ui/activity/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends BaseActivity  {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
     }
 
